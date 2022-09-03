56ff6fa6b774cf1255a06ff28b5369f9ac2b3398
Mike <mllopis11@gmail.com>
2022-09-03 12:56:16 +0200

    [Misc] Rework builder pattern

---
+++ miscellaneous/src/test/java/mike/samples/test/design/patterns/BuilderInterfaceDefaultTest.java
@@ -0,0 +1,74 @@
+package mike.samples.test.design.patterns;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Path;
+import java.util.function.Predicate;
+
+import org.junit.jupiter.api.Test;
+
+class BuilderInterfaceDefaultTest {
+
+    @Test
+    void should_build_FileReader_with_default_charset_and_predicate() {
+        var file = Path.of("foo/bar.txt");
+        var reader = FileReader.create().file(file).iso88591().noFilter();
+        
+        assertThat(reader.getFile()).isEqualTo(file);
+        assertThat(reader.getCharset()).isEqualTo(StandardCharsets.ISO_8859_1);
+        assertThat(reader.getRecordFilter()).isNotNull();
+    }
+}
+
+class FileReader {
+    
+    private final Path file;
+    private final Charset charset;
+    private final Predicate<String> recordFilter;
+    
+    private FileReader(Path file, Charset charset, Predicate<String> recordFilter) {
+        this.file = file;
+        this.charset = charset;
+        this.recordFilter = recordFilter;
+    }
+    
+    public Path getFile() {
+        return file;
+    }
+    
+    public Charset getCharset() {
+        return charset;
+    }
+    
+    public Predicate<String> getRecordFilter() {
+        return recordFilter;
+    }
+    
+    public static Builder create() {
+        return file -> charset -> filter -> new FileReader(file, charset, filter);
+    }
+    
+    interface Builder {
+        
+        ReaderCharset file(Path file);
+        
+        interface ReaderCharset {
+            
+            RecordFilter charset(Charset charset);
+            
+            default RecordFilter iso88591() {
+                return this.charset(StandardCharsets.ISO_8859_1);
+            }
+        }
+        
+        interface RecordFilter {
+            FileReader filter(Predicate<String> recordFilter);
+            
+            default FileReader noFilter() {
+                return this.filter(r -> true);
+            }
+        }
+    }
+}
